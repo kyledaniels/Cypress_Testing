5c2a4e94d2cdc3d5d36f9d30082e9959f8decd5b
kyle67 <daniels.kyle@gmail.com>
2020-03-08 14:07:42 -0400

    Wrote test using wait and pause method

--- cypress/integration/examples/simple.spec.js
+++ cypress/integration/examples/simple.spec.js
@@ -1,13 +1,28 @@
-describe('Browser Actions',()=>{
-    it('Should load correct url',()=>{
-        cy.visit('http://example.com', {timewout:1000})
-    })
+// describe('Browser Actions',()=>{
+//     it('Should load correct url',()=>{
+//         cy.visit('http://example.com', {timewout:1000})
+//     })
 
-    it('Should check correct url', ()=>{
-        cy.url().should('include', 'example.com')
-    })
+//     it('Should check correct url', ()=>{
+//         cy.url().should('include', 'example.com')
+//     })
+
+//     it("should wait for 3 seconds",()=>{
+//         cy.wait(3000)
+//     })
+
+//     it('should pause the execution', ()=>{
+//         cy.pause()
+//     })
     
-    it('should check for current element on the page', ()=>{
-        cy.get('h1').should('be.visible')
-    })
-})
+//     it('should check for current element on the page', ()=>{
+//         cy.get('h1').should('be.visible')
+//     })
+// })
+
+describe("Browser Action",()=>{
+    it('Should load website',()=>{
+      cy.visit('http://books.toscrape.com/',{timeout:3000})
+    });
+});
+
